bbb77bb9e005a4274134843039c20eba4d624e74
Aleshazy <154497496+Aleshazy@users.noreply.github.com>
2024-11-09 14:29:32 +0000

    .

---
+++ Task2/main.c
@@ -0,0 +1,8 @@
+#include "math_operations.h"
+#include "print_operations.h"
+
+int main(){
+
+void print_result();
+return 0;
+}
---
+++ Task2/print_operations.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_OPERATIONS_H
+#define PRINT_OPERATIONS_H
+
+void print_result(void);
+
+#endif // PRINT_OPERATIONS_H
+
+void print_result(int result);
